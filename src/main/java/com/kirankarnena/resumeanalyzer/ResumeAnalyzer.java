8dd42a701fc5e351fd8657e2db9db7024815dbc8
Kirankarnena <saikirankarnena789@gmail.com>
2025-05-14 15:18:49 +0530

    Added Java code

---
+++ src/main/java/com/kirankarnena/resumeanalyzer/ResumeAnalyzer.java
@@ -0,0 +1,7 @@
+package com.kirankarnena.resumeanalyzer;
+
+public class ResumeAnalyzer {
+    public static void main(String[] args) {
+        System.out.println("Hello, Resume Analyzer!");
+    }
+}
